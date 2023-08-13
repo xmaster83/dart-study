185901802a220bb78c0c44d6581b538303a671e2
xmaster83 <xmaster83@gmail.com>
2023-08-13 03:29:58 +0000

    commit for example

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,19 +1,14 @@
-
-
-
-void main (){
-
+void main() {
 //  List<String> spisokFructs = ['appple','orange','cucumber','bananas'];
 //  List<String> spisokSport = ['football','basketball','hokey','tenis']
 
- const spisokFructs = ['appple','orange','cucumber','bananas'];
- const spisokSport = ['football','basketball','hokey','tenis'];
- final spiskStudy= ["terranova", ...spisokSport, ...spisokFructs];
+  const spisokFructs = ['appple', 'orange', 'cucumber', 'bananas'];
+  const spisokSport = ['football', 'basketball', 'hokey', 'tenis'];
+  final spiskStudy = ["terranova", ...spisokSport, ...spisokFructs];
 //  spiskStudy.addAll(spisokSport);
 //  spiskStudy.addAll(spisokFructs);
- print(spiskStudy);
+  print(spiskStudy);
 
- final tes = {"ter",45};
- print(tes)
-
+  final tes = {"ter": 45};
+  print(tes);
 }
