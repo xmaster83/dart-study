10c97636bf74cc727ae85eb31e5a383838fe0346
xmaster83 <xmaster83@gmail.com>
2023-08-26 18:59:59 +0600

    + map

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -11,4 +11,7 @@ void main() {
 
   final tes = {"ter": 45};
   print(tes);
+
+  final Map<int, String> spisokHards = {1:"cisco",2:"microtick",4:"dlink"};
+  final spisokHards2 = {"pc":20,"mac":40,"linux":50};
 }
